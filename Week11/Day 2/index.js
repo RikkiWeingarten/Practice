35f4fe2e9d8e61e1e10e3ce1f80ab1f26bf0ed14
rikkiweingarten <rikkiweingarten@gmail.com>
2024-10-27 16:15:44 +0200

    Added Exercises day 2

---
+++ Week11/Day 2/index.js
@@ -0,0 +1,32 @@
+document.getElementById('message-form-post').onsubmit = function(event) {
+    event.preventDefault();
+    alert('Form submitted!');
+};
+
+const marioGame = {
+    detail : "An amazing game!",
+    characters : {
+        mario : {
+          description:"Small and jumpy. Likes princesses.",
+          height: 10,
+          weight: 3,
+          speed: 12,
+        },
+        bowser : {
+          description: "Big and green, Hates princesses.",
+          height: 16,
+          weight: 6,
+          speed: 4,
+        },
+        princessPeach : {
+          description: "Beautiful princess.",
+          height: 12,
+          weight: 2,
+          speed: 2,
+        }
+    },
+  }
+
+const jsonString = JSON.stringify(marioGame, null, 2); 
+console.log(jsonString);
+// The structure remains the same in json object
